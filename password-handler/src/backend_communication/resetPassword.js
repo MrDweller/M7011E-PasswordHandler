350bc23512cca0c5739e2997495672d1db86bf92
nalle631 <malteolofsson97@gmail.com>
2022-11-30 17:11:02 +0100

    fixed html bug

--- password-handler/src/backend_communication/resetPassword.js
+++ password-handler/src/backend_communication/resetPassword.js
@@ -2,10 +2,11 @@ import RestRequest from '../backend_communication/RestRequest';
 
 export function resetPassword(email) 
 {
+
     let requestData = {};
     requestData["email"] = email;
     requestData["subject"] = 'Request for resetting password for your PasswordHandler account';
-    requestData["msg"] = 'You have requested to reset your password, click this link to be redicrected'
+    requestData["msg"] = 'You have requested to reset your password, click this link to be redicrected';
     console.log(requestData);
     RestRequest.post("localhost", 8080, "/email", requestData, (responseData) => {
         
